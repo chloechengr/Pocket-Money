845312ddfaaeec4fe6cf61566f541b116fbd4fc9
chloechengr <chengsn@bc.edu>
2019-12-01 16:28:27 -0500

    Initial Commit

---
+++ Pocket Money/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pocket Money
+//
+//  Created by Chloe Cheng on 12/1/19.
+//  Copyright © 2019 Chloe Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
